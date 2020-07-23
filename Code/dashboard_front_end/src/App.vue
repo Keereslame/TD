9c45f8c95c583e87a89ed19b30e3df10ca6c69e3
Keereslame <philippine.kazenostame@gmail.com>
2020-07-23 17:45:00 +0200

    Accueil ok, calendar ok

--- Code/dashboard_front_end/src/App.vue
+++ Code/dashboard_front_end/src/App.vue
@@ -1,59 +1,66 @@
 <template>
-  <div id="app">
-    <!--<nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a href="#" class="navbar-brand">Test batch</a>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a href="/batchs" class="nav-link">Batchs</a>
-        </li>
-        <li class="nav-item">
-          <a href="/add" class="nav-link">Add</a>
-        </li>
-      </div>
-    </nav>-->
-    <div>
-      <b-navbar type="dark" variant="dark">
-        <b-navbar-brand class="align-center" href="/">
-          <b-img width="100px" :src="require('../public/name.png')" class="logo" alt="Logo"></b-img>
-        </b-navbar-brand>
+  <b-container fluid>
+    <div id="app">
+        <!--<nav class="navbar navbar-expand navbar-dark bg-dark">
+          <a href="#" class="navbar-brand">Test batch</a>
+          <div class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a href="/batchs" class="nav-link">Batchs</a>
+            </li>
+            <li class="nav-item">
+              <a href="/add" class="nav-link">Add</a>
+            </li>
+          </div>
+        </nav>-->
+        <div>
+          <b-navbar type="dark" variant="dark">
+            <b-navbar-brand class="align-center" href="/">
+              <b-img width="100px" :src="require('../public/name.png')" class="logo" alt="Logo"></b-img>
+            </b-navbar-brand>
 
-        <b-navbar-nav class="ml-auto">
-          <!--<b-nav-item href="user_settings">User settings</b-nav-item>
-          <b-avatar href="profil" class="align-center"></b-avatar>-->
-        </b-navbar-nav>
-      </b-navbar>
+            <b-navbar-nav class="ml-auto">
+              <!--<b-nav-item href="user_settings">User settings</b-nav-item>
+              <b-avatar href="profil" class="align-center"></b-avatar>-->
+            </b-navbar-nav>
+          </b-navbar>
+        </div>
+      <b-row>
+        <b-col class="col-auto sidebar">
+          <div>
+            <b-nav vertical id="sidebar" >
+              <b-nav-item href="/">
+                <b-icon-house-door class="item"></b-icon-house-door>
+              </b-nav-item>
+              <b-nav-item href="/Calendar">
+                <b-icon-calendar3 class="item"></b-icon-calendar3>
+              </b-nav-item>
+              <b-nav-item href="/Data">
+                <b-icon-graph-up class="item"></b-icon-graph-up>
+              </b-nav-item>
+              <b-nav-item href="/Insert">
+                <b-icon-file-earmark-plus class="item"></b-icon-file-earmark-plus>
+              </b-nav-item>
+              <b-nav-item href="/Stock">
+                <b-icon-box class="item"></b-icon-box>
+              </b-nav-item>
+              <b-nav-item href="/Task">
+                <b-icon-pencil-square class="item"></b-icon-pencil-square>
+              </b-nav-item>
+              <b-nav-item href="/Measure">
+                <b-icon-droplet-half class="item"></b-icon-droplet-half>
+              </b-nav-item>
+            </b-nav>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="container mt-3">
+            <router-view />
+          </div>
+        </b-col>
+      </b-row>
     </div>
+  </b-container>
 
-    <div>
-      <b-nav vertical id="sidebar" >
-        <b-nav-item href="/">
-          <b-icon-house-door class="item"></b-icon-house-door>
-        </b-nav-item>
-        <b-nav-item href="/Calendar">
-          <b-icon-calendar3 class="item"></b-icon-calendar3>
-        </b-nav-item>
-        <b-nav-item href="/Data">
-          <b-icon-graph-up class="item"></b-icon-graph-up>
-        </b-nav-item>
-        <b-nav-item href="/Insert">
-          <b-icon-file-earmark-plus class="item"></b-icon-file-earmark-plus>
-        </b-nav-item>
-        <b-nav-item href="/Stock">
-          <b-icon-box class="item"></b-icon-box>
-        </b-nav-item>
-        <b-nav-item href="/Task">
-          <b-icon-pencil-square class="item"></b-icon-pencil-square>
-        </b-nav-item>
-        <b-nav-item href="/Measure">
-          <b-icon-droplet-half class="item"></b-icon-droplet-half>
-        </b-nav-item>
-      </b-nav>
-    </div>
-
-    <div class="container mt-3">
-      <router-view />
-    </div>
-  </div>
 </template>
 
 <script>
@@ -64,12 +71,26 @@
 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .container-fluid{
+    padding-right: 0px;
+    padding-left: 0px;
+  }
+  .col-auto{
+    background-color: #96d2d4;
+    padding-right: 0px;
+  }
+
+  .nav-link{
+    color: #2c3e50;
+  }
+
 </style>
--- Code/dashboard_front_end/src/layout/Calendar.vue
+++ Code/dashboard_front_end/src/layout/Calendar.vue
@@ -7,26 +7,48 @@
                 <div class="mt-3">
                     <b-card-group deck class="mb-3">
                         <b-card border-variant="light" class="text-center">
-                            <b-calendar v-model="value" locale="fr-EU"></b-calendar>                        </b-card>
+                            <b-calendar v-model="value" locale="fr-EU" :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }" @context="retrieveTodayBatchs"></b-calendar>                        </b-card>
 
                         <b-card border-variant="light" header="Task" class="text-center">
                             <b-row>
                                 <p>Date: <b>{{ value }}</b></p>
                             </b-row>
                             <b-row>
-                                <h4>Feeding</h4>
+                                <h4>Feeding during reproduction</h4>
                             </b-row>
                             <b-row>
-                                <p>Batch:</p>
+                                <li class="list-group-item"
+                                    v-for="(batch, index) in reproduction_feeding_today"
+                                    :key="index"
+                                >
+                                    Batch:{{ batch.name }},
+                                    quantity:{{ batch.reproduction_humidity_mass }},
+                                    type: {{ batch.reproduction_humidity_type }}
+                                </li>
                             </b-row>
                             <b-row>
-                                <p>Amount:</p>
+                                <h4>Feeding during growth</h4>
+                            </b-row>
+                            <b-row>
+                                <li class="list-group-item"
+                                    v-for="(batch, index) in growth_feeding_today"
+                                    :key="index"
+                                >
+                                    Batch:{{ batch.name }},
+                                    quantity:{{ batch.growth_humidity_mass }},
+                                    type: {{ batch.growth_humidity_type }}
+                                </li>
                             </b-row>
                             <b-row>
                                 <h4>Harvest</h4>
                             </b-row>
                             <b-row>
-                                <p>Batch:</p>
+                                <li class="list-group-item"
+                                    v-for="(batch, index) in harvest_today"
+                                    :key="index"
+                                >
+                                    {{ batch.name }}
+                                </li>
                             </b-row>
                         </b-card>
                     </b-card-group>
@@ -40,19 +62,94 @@
     //import Navbar from "../components/Navbar";
     //import Sidebar from "../components/Sidebar";
 
+    import BatchDataService from "../services/BatchDataService";
+
     export default {
         name: "Calendar",
         components: {
             //Sidebar,
             //Navbar,
         },
-        data(){
-            return{
-                value:'',
-
-            }
+        data() {
+            return {
+                value: '',
+                batchs: [],
+                reproduction_feeding_today: [],
+                growth_feeding_today: [],
+                harvest_today: [],
+                name: ""
+            };
         },
-        methods:{
+        methods: {
+            retrieveBatchs() {
+                BatchDataService.getAll()
+                    .then(response => {
+                        this.batchs = response.data;
+                        //console.log(response.data);
+                    })
+                    .catch(e => {
+                        console.log(e);
+                    });
+            },
+            retrieveTodayBatchs() {
+                this.reproduction_feeding_today = [];
+                this.growth_feeding_today = [];
+                this.harvest_today = [];
+                //console.log(new Date(this.value))
+                var day_to_ms = 8.64*Math.pow(10, 7);
+                for(var i in this.batchs){
+                    //console.log("Batch: " + this.batchs[i].name);
+                    var date_start = new Date(this.batchs[i].date_start);
+                    //console.log("Date start: " + date_start);
+                    var date_end_laying = new Date(date_start.getTime()+this.batchs[i].nb_laying_day*day_to_ms);
+                    //console.log("Date end laying: " + date_end_laying);
+                    var date_start_growth = new Date(date_end_laying.getTime()+this.batchs[i].humidity_delay*day_to_ms);
+                    //console.log("Date start growth: ");
+                    //console.log(new Date(date_start_growth));
+                    var date_end_growth = new Date(date_start_growth.getTime() + this.batchs[i].growth_day*day_to_ms);
+                    //console.log(this.batchs[i].growth_day)
+                    //console.log("Date end growth: ");
+                    //console.log(new Date(date_end_growth));
+
+                    for(let index=date_start; index<date_end_laying;index = new Date(index.getTime()+this.batchs[i].reproduction_humidity_freq*day_to_ms))//index+=this.batchs[i].reproduction_humidity_freq)
+                    {
+                        //console.log(index.getTime());
+                        //console.log(new Date(this.value).getTime())
+                        if(index.getTime() == new Date(this.value).getTime())
+                        {
+                            this.reproduction_feeding_today.push(this.batchs[i])
+                            console.log("Reproduction feeding date")
+                        }
+                    }
+
+
+                    //console.log("Start growth " + new Date(date_start_growth));
+                    //console.log("End growth " + new Date(date_end_growth));
+                    for(let index=date_start_growth; index<date_end_growth;index = new Date(index.getTime() + this.batchs[i].growth_humidity_freq * day_to_ms))//index+=this.batchs[i].growth_humidity_freq)
+                    {
+                        if(date_end_growth>new Date()) {
+                            //console.log("index " + new Date(index));
+                            if (index.getTime() == new Date(this.value).getTime()) {
+                                this.growth_feeding_today.push(this.batchs[i])
+                                console.log("Growth feeding date")
+                            }
+                        }
+                    }
+                    //console.log(date_end_growth)
+                    if(date_end_growth.getTime() == new Date(this.value).getTime())
+                    {
+                        this.harvest_today.push(this.batchs[i])
+                        console.log("Harvest date")
+                    }
+                }
+                //console.log(this.feeding_today)
+            }
+
+        },
+        mounted() {
+            this.retrieveBatchs();
+        },
+        watch:{
 
         }
     }
